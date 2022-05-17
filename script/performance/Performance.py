126651ab0b91a4932bf520bf622625d5c6989db3
DarrenH8848 <DarrenH8848@outlook.com>
2022-05-17 23:48:37 +0800

    seven new performance functions and some new tests

--- script/performance/Performance.py
+++ script/performance/Performance.py
@@ -1,6 +1,9 @@
+from cmath import log, sqrt
+from re import I
 import pandas as pd
 import numpy as np
-from .Constant import *
+from .Constant import ANNUALIZATION_FACTORS, DAYS_PER_YEAR
+from .Constant import DAILY, WEEKLY, MONTHLY, QUARTERLY, YEARLY
 
 
 def AnnualizedFactor(period):
@@ -14,32 +17,202 @@ def AnnualizedFactor(period):
 
 
 def AnnualizedReturn(returns, period = DAILY, logreturn = False):
-    returns.dropna(how = 'any', inplace = True)
+    """
+    Calculate the nannualized return
+
+    Args:
+        returns (pd.Series, pd.Dataframe): Periodical returns time series data.
+        period (str, optional): Defines the periodicity of the 'returns' data.
+         Defaults to DAILY. Also can be "weekly", "monthly", "quarterly" or "yearly".
+        logreturn (bool, optional): Define the type of returns.
+         False for simple return, True for log return. Defaults to False.
+
+    Returns:
+        float, pd.Series: Annualized return for each time series.
+    """    
+    returns.dropna(how = 'all', inplace = True)
     if len(returns) < 1:
-        return np.nan
+        return np.full([1,returns.ndim], np.nan)
 
     annual_factor = AnnualizedFactor(period)
-    returns_length = len(returns)
-    if logreturn:
-        result = np.mean(returns) * annual_factor
+    if isinstance(returns, pd.DataFrame):
+        returns_length = returns.count(axis = 0)
     else:
-        prod_return = (returns+1).prod()
+        returns_length = len(returns)
+    if logreturn:
+        result = np.nanmean(returns, axis = 0) * annual_factor
+    else:
+        prod_return = np.nanprod((returns + 1).values, axis = 0)
         result = prod_return**(annual_factor/returns_length)-1
 
     return result
+
     
 def AnnualizedSD(returns, period = DAILY):
-    returns.dropna(how = 'any', inplace = True)
-    if len(returns) < 1:
-        return np.nan
+    """
+    Calculate the nannualized standard deviation
+
+    Args:
+        returns (pd.Series, pd.Dataframe): Periodical returns time series data.
+        period (str, optional): Defines the periodicity of the 'returns' data.
+         Defaults to DAILY. Also can be "weekly", "monthly", "quarterly" or "yearly".
+
+    Returns:
+        float, pd.Series: Annualized standard deviation for each time series.
+    """    
+    returns.dropna(how = 'all', inplace = True)
+    if len(returns) < 2:
+        return np.full([1,returns.ndim], np.nan)
     
     annual_factor = AnnualizedFactor(period)
-    result = np.std(returns, axis = 0 ) * np.sqrt(annual_factor)
+    if isinstance(returns, pd.DataFrame):
+        result = np.nanstd(returns, axis = 0, ddof = 1) * np.sqrt(annual_factor)
+    else:
+        result = np.nanstd(returns, ddof = 1) * np.sqrt(annual_factor)
+    return result
+
+def CumulativeReturns(returns, logreturn = False):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        logreturn (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+    returns.dropna(how = 'all', inplace = True)
+    if len(returns) < 1:
+        return returns.copy()
+    
+    if logreturn:
+        result = returns.cumsum(axis = 0)
+    else:
+        result = returns.copy() + 1
+        result = result.cumprod(axis = 0) - 1
 
     return result
 
-# df = pd.read_csv('./out/matrix_return_data.csv')
-# df.set_index(['Date'],inplace=True)
-# print(AnnualizedReturn(df))
+def Drawdown(returns, logreturn = False):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        logreturn (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+    if len(returns) < 1:
+        return np.full([1,returns.ndim], np.nan)
+    
+    cumulativereturns = CumulativeReturns(returns, logreturn) + 1
+    maxcumulativereturns = cumulativereturns.cummax(axis = 0)
+    result = (cumulativereturns - maxcumulativereturns) / maxcumulativereturns
+    
+    return result
+
+def AverageDrawDown(returns, logreturn = False):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        logreturn (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+    if len(returns) < 1:
+        return np.full([1,returns.ndim], np.nan)
+
+    drawdown = Drawdown(returns, logreturn)
+    if isinstance(drawdown, pd.DataFrame):
+        result = pd.DataFrame(index = drawdown.index, columns = drawdown.columns)
+        for asset in drawdown.columns:
+            for i in range(len(drawdown)):
+                result[asset][i] = np.nanmean(drawdown[asset][:i+1])
+    else:
+        result = pd.Series(index = drawdown.index)
+        for j in range(len(drawdown)):
+            result[j] = np.nanmean(drawdown[:j+1])
+    return result
+    
+def MaxDrawdown(returns, logreturn = False):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        logreturn (bool, optional): _description_. Defaults to False.
+    """
+    if len(returns) < 1:
+        return np.full([1,returns.ndim], np.nan)
+
+    result = Drawdown(returns, logreturn)
+    result = result.min(axis = 0)
+    return result
+
+def SharpeRatio(returns, risk_free = 0, logreturn = False, period = DAILY):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        risk_free (int, optional): _description_. Defaults to 0.
+        logreturn (bool, optional): _description_. Defaults to False.
+        period (_type_, optional): _description_. Defaults to DAILY.
+
+    Returns:
+        _type_: _description_
+    """    
+    returns.dropna(how = 'all', inplace = True)
+    if len(returns) < 2:
+        return np.full([1,returns.ndim], np.nan)
+    
+    result = np.divide(AnnualizedReturn(returns, period, logreturn)-risk_free, AnnualizedSD(returns, period))
+    return result
+ 
+def CalmarRatio(returns, period = DAILY, logreturn = False):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        period (_type_, optional): _description_. Defaults to DAILY.
+        logreturn (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+    max_drawdown = MaxDrawdown(returns, logreturn)
+    result = AnnualizedReturn(returns, period, logreturn) / abs(max_drawdown)
+    result = result.replace([np.inf, -np.inf], np.nan)
+    return result
+
+def BurkeRatio(returns, risk_free = 0, period = DAILY, logreturn = False, modified = False):
+    """
+    _summary_
+
+    Args:
+        returns (_type_): _description_
+        risk_free (int, optional): _description_. Defaults to 0.
+        period (_type_, optional): _description_. Defaults to DAILY.
+        logreturn (bool, optional): _description_. Defaults to False.
+        modified (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """    
+    rp = AnnualizedReturn(returns, period, logreturn)
+    drawdown = Drawdown(returns, logreturn)
+    result = (rp - risk_free) / np.sqrt(np.nansum(drawdown**2, axis = 0))
+    if modified:
+        result = result * np.sqrt(drawdown.count(axis = 0))
+    result = result.replace([np.inf, -np.inf], np.nan)
+    return result
+
 
 
--- script/test_func.py
+++ script/test_func.py
@@ -1,22 +1,30 @@
+from operator import index
+from re import S
 import pytest 
 import pandas as pd
 import numpy as np
+from pandas import testing as tm
 from performance import Performance
-from performance import add
 from performance import Constant
 
 
-# daily_returns = pd.Series(
-#     np.array([np.nan, 1., 10., -4., 2., 3., 2., 1., -10.]) / 100,
-#     index=pd.date_range('2000-1-30', periods=9, freq='D'))
-
 daily_returns = pd.Series(
-    np.array([0., 1., 10., -4., 2., 3., 2., 1., -10.]) / 100,
+    np.array([np.nan, 1., 10., -4., 2., 3., 2., 1., -10.]) / 100,
     index=pd.date_range('2000-1-30', periods=9, freq='D'))
+
+# daily_returns = pd.Series(
+#     np.array([0., 1., 10., -4., 2., 3., 2., 1., -10.]) / 100,
+#     index=pd.date_range('2000-1-30', periods=9, freq='D'))
 
 asset1 = [0., 1., 10., -4., 2., 3., 2., 1., -10.]
 asset2 = [1., 4., 2., -3., 1., 4., 1., 0., -8.]
 daily_returns_matrix = pd.DataFrame(
+    np.array([asset1, asset2]).T / 100, columns = ['asset1', 'asset2'],
+    index = pd.date_range('2000-1-30', periods=9, freq='D'))
+
+asset1 = [np.nan, 1., 10., -4., 2., 3., 2., 1., -10.]
+asset2 = [np.nan, np.nan, 2., -3., 1., 4., 1., 0., -8.]
+daily_returns_matrix_withnan = pd.DataFrame(
     np.array([asset1, asset2]).T / 100, columns = ['asset1', 'asset2'],
     index = pd.date_range('2000-1-30', periods=9, freq='D'))
 
@@ -36,24 +44,25 @@ empty_returns = pd.Series(
     np.array([])/100,
     index=pd.date_range('2000-1-30', periods=0, freq='D'))
 
+asset1 = []
+asset2 = []
+empty_returns_matrix = pd.DataFrame(
+    np.array([asset1, asset2]).T / 100, columns = ['asset1', 'asset2'],
+    index = pd.date_range('2000-1-30', periods=0, freq='D'))
+
 flat_returns = pd.Series(
         np.linspace(0.01, 0.01, num=100),
         index=pd.date_range('2000-1-30', periods=100, freq='D')
     )
 
-MAXERROR = 10e-8
+MAXERROR = 10e-5
 
-@pytest.mark.parametrize(
-    "x,y,expected",
-    [
-        (1,1,2),
-        (2,2,4),
-        (10,10,20),
-    ]
-)
-def test_add(x,y,expected):
-    assert add.add(x,y) == expected
-
+def list_to_series_or_dataframe(expect):
+    array = np.array(expect)
+    if array.ndim > 1:
+        return pd.DataFrame(np.array(expect),columns = ['asset1', 'asset2'])
+    else:
+        return pd.Series(np.array(expect), index = ['asset1', 'asset2'])
 
 @pytest.mark.parametrize(
     "x,y,z,expected",
@@ -64,14 +73,16 @@ def test_add(x,y,expected):
         (one_return, Constant.DAILY, False, 15.173072621219674),
         (weekly_returns, Constant.WEEKLY, True, 0.288888888888889),
         (empty_returns, Constant.DAILY, False, np.nan),
+        (empty_returns_matrix, Constant.DAILY, False, [[np.nan, np.nan]]),
         (daily_returns_matrix, Constant.DAILY, False, [1.9135925373194578,0.4905101332607782]),
     ]
 )
 def test_AnnualizedReturn(x,y,z,expected):
     if np.isnan(expected).all():
-        assert np.isnan(Performance.AnnualizedReturn(x,y,z))
+        assert np.isnan(Performance.AnnualizedReturn(x,y,z)).all()
     elif isinstance(x, pd.DataFrame):
         assert (Performance.AnnualizedReturn(x,y,z) - expected < MAXERROR).all()
+        # tm.assert_series_equal(Performance.AnnualizedReturn(x,y,z),expected)
     else:
         assert Performance.AnnualizedReturn(x,y,z) - expected < MAXERROR
 
@@ -84,7 +95,7 @@ def test_AnnualizedReturn(x,y,z,expected):
         (weekly_returns, Constant.WEEKLY, 0.38851569394870583),
         (monthly_returns, Constant.MONTHLY, 0.18663690238892558),
         (empty_returns, Constant.DAILY, np.nan),
-        (daily_returns_matrix, Constant.DAILY, [0.8063635794244568,0.5588877844035917]),
+        (daily_returns_matrix, Constant.DAILY, [0.85527773,0.59279001]),
     ]
 )
 def test_AnnualizedSD(x,y,expected):
@@ -95,6 +106,69 @@ def test_AnnualizedSD(x,y,expected):
     else:
         assert Performance.AnnualizedSD(x,y) - expected < MAXERROR
 
+@pytest.mark.parametrize(
+    "x,y,expected",
+    [
+        (daily_returns, False, pd.Series([0.01, 0.111, 0.066560, 0.087891, 0.120528, 0.142938, 0.154368, 0.038931])),
+        (empty_returns, False, pd.Series([])),
+        (daily_returns_matrix, False, pd.DataFrame([[0.0, 0.01, 0.111, 0.066559, 0.08789, 0.12052,
+                            0.14293, 0.15436, 0.03893],[0.01, 0.0504, 0.071408, 0.039266, 0.049658, 0.091645, 0.102561, 0.102561, 0.014356]]).T),
+        (daily_returns_matrix_withnan, False, pd.DataFrame([[0.01, 0.111, 0.066559, 0.08789, 0.12052,
+                            0.14293, 0.15436, 0.03893],[np.nan, 0.02, -0.0106, -0.000706, 0.039266, 0.049658, 0.049658, -0.034314]]).T),
+    ]
+)
+def test_CumulativeReturns(x, y, expected):
+    cum_returns = Performance.CumulativeReturns(x, y)
+    if isinstance(x, pd.DataFrame):
+        expected.index =  cum_returns.index
+        expected.columns = cum_returns.columns
+        tm.assert_frame_equal(cum_returns, expected, atol = MAXERROR)
+    else:
+        expected.index = cum_returns.index
+        tm.assert_series_equal(cum_returns, expected, atol = MAXERROR)
 
-print(Performance.AnnualizedSD(daily_returns, Constant.DAILY))
+# todo: more tests following functions
+@pytest.mark.parametrize(
+    "x,y,expected",
+    [
+        (daily_returns, False, pd.Series([0., 0., -0.04, -0.0208, 0., 0., 0., -0.1])),
+        (daily_returns_matrix, False, pd.DataFrame([[0., 0., 0., -0.04, -0.0208, 0.,
+                            0., 0., -0.1],[0., 0., 0., -0.03, -0.0203, 0., 0., 0., -0.08]]).T),
+        (daily_returns_matrix_withnan, False, pd.DataFrame([[0., 0., -0.04, -0.0208, 0.,
+                            0., 0., -0.1],[np.nan, 0., -0.03, -0.0203, 0., 0., 0., -0.08]]).T),
+    ]
+)
+def test_Drawdown(x, y, expected):
+    drawdown = Performance.Drawdown(x, y)
+    if isinstance(x, pd.DataFrame):
+        expected.index = drawdown.index
+        expected.columns = drawdown.columns
+        tm.assert_frame_equal(drawdown, expected, check_dtype = False, atol = MAXERROR)
+    else:
+        expected.index = drawdown.index
+        tm.assert_series_equal(drawdown, expected, check_dtype = False, atol = MAXERROR)
+
+@pytest.mark.parametrize(
+    "x,y,expected",
+    [
+        (daily_returns, False, pd.Series([0., 0., -0.013333, -0.015200, -0.012160, -0.010133, -0.008686, -0.020100])),
+        (daily_returns_matrix, False, pd.DataFrame([[0., 0., 0., -0.01, -0.01216, -0.010133, -0.008686, -0.0076, -0.017867],
+                                            [0., 0., 0., -0.0075, -0.01006, -0.008383, -0.007186, -0.006288, -0.014478]]).T),
+        (daily_returns_matrix_withnan, False, pd.DataFrame([[0., 0., -0.013333, -0.0152, -0.01216, -0.010133, -0.008686, -0.0201],
+                                                    [np.nan, 0., -0.015, -0.016767, -0.012575, -0.01006, -0.008383, -0.018614]]).T),
+    ]
+)
+def test_AverageDrawDown(x, y, expected):
+    avg_drawdown = Performance.AverageDrawDown(x, y)
+    if isinstance(x, pd.DataFrame):
+        expected.index = avg_drawdown.index
+        expected.columns = avg_drawdown.columns
+        tm.assert_frame_equal(avg_drawdown, expected, check_dtype = False, atol = MAXERROR)
+    else:
+        expected.index = avg_drawdown.index
+        tm.assert_series_equal(avg_drawdown, expected, check_dtype = False, atol = MAXERROR)
+
+
+print(Performance.AverageDrawDown(daily_returns_matrix_withnan, False))
+
 
