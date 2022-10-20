stat: cdf_kernel & tail idx; risk: tail_dependence & coeff; arr_bcmk: manual broadcast; yapf; bugfix: assert

scripts/risk.py
@@ -1,10 +1,14 @@
-from numpy import (apply_along_axis, array, isfinite, nanmean, nanstd,
-                   quantile, sort, sqrt, square, unique, vectorize)
+from itertools import cycle
+
+from numpy import (apply_along_axis, array, exp, isfinite, log, maximum,
+                   nanmean, nanstd, quantile, sort, sqrt, square, unique,
+                   vectorize)
 from scipy.interpolate import CubicSpline
 from scipy.special import ndtr
 from scipy.stats import norm, t
 
 from .capm import beta_capm
+from .stat import cdf_func_kernel
 
 
 def downside_dev(arr_ts: array, lst_idx_retn: list,
@@ -205,22 +209,78 @@ def tracking_error(arr_ts: array,
     return te
 
 
-# TODO td
+def _tail_dependence_lower(vec_retn: array, vec_bcmk: array,
+                           alpha: float) -> float:
+    idx = vec_bcmk <= quantile(a=vec_bcmk, q=alpha, method='median_unbiased')
+    return ((vec_retn[idx] <= quantile(
+        a=vec_retn, q=alpha, method='median_unbiased')).sum() / idx.sum())
+
+
+def _tail_dependence_upper(vec_retn: array, vec_bcmk: array,
+                           alpha: float) -> float:
+    q = 1 - alpha
+    idx = vec_bcmk >= quantile(a=vec_bcmk, q=q, method='median_unbiased')
+    return ((vec_retn[idx] >= quantile(
+        a=vec_retn, q=q, method='median_unbiased')).sum() / idx.sum())
+
+
 def tail_dependence(arr_ts: array,
                     lst_idx_retn: list,
                     lst_idx_bcmk: list,
-                    flag_lower: bool = True,
-                    alpha: float = 1.0) -> array:
-    pass
+                    alpha: float,
+                    flag_lower: bool = True) -> array:
+    arr_retn = arr_ts[:, lst_idx_retn]
+    arr_bcmk = arr_ts[:, lst_idx_bcmk]
+    tmp_len = len(lst_idx_retn)
+    if (tmp_len > 1) and (len(lst_idx_bcmk) < 2):
+        arr_bcmk = arr_bcmk.repeat(tmp_len, axis=1)
+    if flag_lower:
+        return array((*map(_tail_dependence_lower, arr_retn.T, arr_bcmk.T,
+                           cycle([alpha])), ),
+                     ndmin=2)
+    else:
+        return array((*map(_tail_dependence_upper, arr_retn.T, arr_bcmk.T,
+                           cycle([alpha])), ),
+                     ndmin=2)
 
 
-# TODO td
+def _tail_dependence_coefficient_FF(vec_i: array, vec_j: array):
+    return 3.0 - 1.0 / (1.0 - nanmean(
+        maximum(cdf_func_kernel(vec_i)(vec_i),
+                cdf_func_kernel(vec_j)(vec_j)).clip(min=0.0, max=1.0)))
+
+
+def _tail_dependence_coefficient_CFG(vec_i: array, vec_j: array):
+    ecdf_i = cdf_func_kernel(vec_i)(vec_i)
+    ecdf_j = cdf_func_kernel(vec_j)(vec_j)
+    p_max = maximum(ecdf_i, ecdf_j).clip(min=0.0, max=1.0)
+    return 2.0 - 2.0 * exp(
+        nanmean(log(sqrt(log(ecdf_i) * log(ecdf_j)) / (-2.0 * log(p_max)))))
+
+
 def tail_dependence_coefficient(arr_ts: array,
                                 lst_idx_retn: list,
                                 lst_idx_bcmk: list,
                                 flag_lower: bool = True,
                                 method: str = "FF") -> array:
-    pass
+    arr_retn = arr_ts[:, lst_idx_retn]
+    arr_bcmk = arr_ts[:, lst_idx_bcmk]
+    tmp_len = len(lst_idx_retn)
+    if (tmp_len > 1) and (len(lst_idx_bcmk) < 2):
+        arr_bcmk = arr_bcmk.repeat(tmp_len, axis=1)
+    if flag_lower:
+        arr_retn = -arr_retn
+        arr_bcmk = -arr_bcmk
+    if method == 'FF':
+        return array(
+            (*map(_tail_dependence_coefficient_FF, arr_retn.T, arr_bcmk.T), ),
+            ndmin=2)
+    elif method == 'CFG':
+        return array(
+            (*map(_tail_dependence_coefficient_CFG, arr_retn.T, arr_bcmk.T), ),
+            ndmin=2)
+    else:
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
@@ -231,80 +291,112 @@ if __name__ == '__main__':
     arr = data_test.values
     N = data_test.shape[1] - 2
     #
-    tmp = downside_dev(arr_ts=arr, lst_idx_retn=range(N),
-                       lst_idx_bcmk=[N]).shape
+    tmp = downside_dev(arr_ts=arr, lst_idx_retn=range(N), lst_idx_bcmk=[N])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='downside_dev')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
-    tmp = downside_var(arr_ts=arr, lst_idx_retn=range(N),
-                       lst_idx_bcmk=[N]).shape
+    tmp = downside_var(arr_ts=arr, lst_idx_retn=range(N), lst_idx_bcmk=[N])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='downside_var')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
     tmp = downside_potential(arr_ts=arr,
                              lst_idx_retn=range(N),
-                             lst_idx_bcmk=[N]).shape
+                             lst_idx_bcmk=[N])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='downside_potential')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
-    tmp = downside_freq(arr_ts=arr, lst_idx_retn=range(N),
-                        lst_idx_bcmk=[N]).shape
+    tmp = downside_freq(arr_ts=arr, lst_idx_retn=range(N), lst_idx_bcmk=[N])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='downside_freq')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
     for method in ['hist', 'kernel', 'norm', 't']:
         tmp = value_at_risk(arr_ts=arr,
                             lst_idx_retn=range(N),
                             alpha=.5,
-                            method=method).shape
+                            method=method)
+        assert isfinite(tmp).all()
+        tmp = tmp.shape
         logging.log(level=logging.INFO, msg=f'value_at_risk: {method}')
         logging.log(level=logging.INFO, msg=tmp)
         assert tmp == (1, 14)
-        assert isfinite(tmp).all()
         tmp = expected_shortfall(arr_ts=arr,
                                  lst_idx_retn=range(N),
                                  alpha=.5,
-                                 method=method).shape
+                                 method=method)
+        assert isfinite(tmp).all()
+        tmp = tmp.shape
         logging.log(level=logging.INFO, msg=f'expected_shortfall: {method}')
         logging.log(level=logging.INFO, msg=tmp)
         assert tmp == (1, 14)
-        assert isfinite(tmp).all()
     #
-    tmp = beta_fama(arr_ts=arr, lst_idx_retn=range(N), lst_idx_bcmk=[N]).shape
+    tmp = beta_fama(arr_ts=arr, lst_idx_retn=range(N), lst_idx_bcmk=[N])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='beta_fama')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
     tmp = mean_absolute_dev(
         arr_ts=arr,
         lst_idx_retn=range(N),
-    ).shape
+    )
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='mean_absolute_dev')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
     tmp = risk_systematic(arr_ts=arr,
                           lst_idx_retn=range(N),
                           lst_idx_bcmk=[N],
-                          lst_idx_rf=[N + 1]).shape
+                          lst_idx_rf=[N + 1])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='risk_systematic')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
     #
-    tmp = tracking_error(arr_ts=arr, lst_idx_retn=range(N),
-                         lst_idx_bcmk=[N]).shape
+    tmp = tracking_error(arr_ts=arr, lst_idx_retn=range(N), lst_idx_bcmk=[N])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='tracking_error')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 14)
-    assert isfinite(tmp).all()
+    #
+    for flag_lower in (True, False):
+        tmp = tail_dependence(arr_ts=arr,
+                              lst_idx_retn=range(N),
+                              lst_idx_bcmk=[N],
+                              alpha=.1,
+                              flag_lower=flag_lower)
+        assert isfinite(tmp).all()
+        tmp = tmp.shape
+        logging.log(level=logging.INFO, msg=f'tail_dependence: {flag_lower}')
+        logging.log(level=logging.INFO, msg=tmp)
+        assert tmp == (1, 14)
+        #
+    for flag_lower in (True, False):
+        for method in ['FF', 'CFG']:
+            tmp = tail_dependence_coefficient(arr_ts=arr,
+                                              lst_idx_retn=range(N),
+                                              lst_idx_bcmk=[N],
+                                              flag_lower=flag_lower,
+                                              method=method)
+            assert isfinite(tmp).all()
+            tmp = tmp.shape
+            logging.log(level=logging.INFO,
+                        msg=f'tail_dependence: {flag_lower}: {method}')
+            logging.log(level=logging.INFO, msg=tmp)
+            assert tmp == (1, 14)

scripts/stat.py
@@ -1,5 +1,23 @@
 # TODO: hypothesis tests: stationarity, unit root, cointegration
-from numpy import array, log, nanmean, sqrt, square
+
+from itertools import cycle
+
+from numpy import (array, ceil, exp, geomspace, int0, isfinite, log, nanmean,
+                   nanstd, random, sqrt, square, unique, vectorize)
+from scipy.special import ndtr
+
+
+def cdf_func_kernel(vec: array):
+    # * spline inside ppf needs sort; cdf no need
+    vec_clean = vec[isfinite(vec)]
+    band_width = nanstd(vec_clean) * 0.6973425390765554 * (len(vec_clean))**(
+        -0.1111111111111111)
+
+    @vectorize
+    def cdf_k(q: array) -> array:
+        return ndtr((q - vec_clean) / band_width).mean()
+
+    return cdf_k
 
 
 def kurtosis(arr_retn: array) -> array:
@@ -54,6 +72,55 @@ def index_pain(arr_retn: array) -> array:
                arr_retn.std(axis=0, keepdims=True)) / log(len(arr_retn))
 
 
+def _index_tail_upper(vec: array, len_sample_size: int, num_sim: int) -> float:
+    vec_clean = vec[vec > 0]
+    vec_clean = vec_clean[isfinite(vec_clean)]
+    vec_sample_size = len(vec_clean) * 0.6321205588285577
+    vec_sample_size = unique(
+        int0(
+            ceil(geomspace(start=1, stop=vec_sample_size,
+                           num=len_sample_size))))
+
+    @vectorize
+    def _MPMR(sample_size: int) -> float:
+
+        @vectorize
+        def _subsample_blockmaxima_log(seed: int) -> float:
+            random.seed(seed=seed)
+            return max(
+                random.choice(a=vec_clean, size=sample_size, replace=False))
+
+        vec = log(_subsample_blockmaxima_log(range(num_sim)))
+        band_width = nanstd(vec) * 0.6973425390765554 * (vec.size)**(
+            -0.1111111111111111)
+        mode_0 = 1.0
+        for _ in range(1000):
+            mode_1 = mode_0
+            tmp = exp(-square(vec - mode_1) / band_width).clip(min=1e-16)
+            mode_0 = (vec @ tmp) / tmp.sum()
+            if abs(mode_0 - mode_1) < 1e-8:
+                break
+        return mode_0
+
+    ydev = _MPMR(vec_sample_size)
+    ydev -= ydev.mean()
+    xdev = log(vec_sample_size)
+    xdev -= xdev.mean()
+    return square(xdev).sum() / (xdev @ ydev)
+
+
+def index_tail(arr_retn: array,
+               flag_lower: bool = True,
+               len_sample_size: int = 11,
+               num_sim: int = 600) -> array:
+    if flag_lower:
+        arr_retn = -arr_retn
+    arr_retn = arr_retn.clip(min=0)
+    return array((*map(_index_tail_upper, arr_retn.T, cycle([len_sample_size]),
+                       cycle([num_sim])), ),
+                 ndmin=2)
+
+
 # TODO rsp
 # ! https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1821643
 def min_TRL(arr_retn: array) -> array:
@@ -69,20 +136,38 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     arr = data_test.values
     #
-    tmp = kurtosis(arr).shape
+    tmp = cdf_func_kernel(arr[:, [0]])(arr[:, [0]])
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
+    logging.log(level=logging.INFO, msg='get_ecdf_func')
+    logging.log(level=logging.INFO, msg=tmp)
+    assert tmp == (1542, 1)
+    #
+    tmp = kurtosis(arr)
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='kurtosis')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 16)
-    assert isfinite(tmp).all()
     #
-    tmp = skewness(arr).shape
+    tmp = skewness(arr)
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='skewness')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 16)
-    assert isfinite(tmp).all()
     #
-    tmp = index_hurst(arr).shape
+    tmp = index_hurst(arr)
+    assert isfinite(tmp).all()
+    tmp = tmp.shape
     logging.log(level=logging.INFO, msg='index_hurst')
     logging.log(level=logging.INFO, msg=tmp)
     assert tmp == (1, 16)
-    assert isfinite(tmp).all()
+    #
+    for flag_lower in [True, False]:
+        tmp = index_tail(arr, flag_lower=flag_lower)
+        assert isfinite(tmp).all()
+        tmp = tmp.shape
+        logging.log(level=logging.INFO, msg=f'index_tail: {flag_lower}')
+        logging.log(level=logging.INFO, msg=tmp)
+        assert tmp == (1, 16)